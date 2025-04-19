b064ed087cce4333dc2283c0b6ffaeec56991eb1
Waqar Khan <waqar.mrwt424@gmail.com>
2025-04-19 13:16:52 +0500

    fix completed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.watchOptions = {
+      ignored: ['**/Application Data/**'],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
